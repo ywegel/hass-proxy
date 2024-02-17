26ef44401cb01a58bfa0ba145a53a4648c22d6c0
DerPizzaBoi <dev@pizzaboi.de>
2024-02-17 17:08:42 +0100

    hello world

---
+++ README.md
@@ -0,0 +1,4 @@
+# HASS Proxy
+
+This is a simple proxy server that allows you to access data of your Home Assistant 
+instance from outside your network. 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(index));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .expect("Failed to bind to address");
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
