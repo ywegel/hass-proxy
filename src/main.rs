3c1ad54b87004cd1efc84ff0ab453a553b829e59
DerPizzaBoi <dev@pizzaboi.de>
2024-02-17 20:45:49 +0100

    #1: tracing and .env configuration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
 use axum::{response::Html, routing::get, Router};
+use serde::Deserialize;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(index));
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::filter::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                "hass_proxy=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+    tracing::debug!("Tracing initiated");
+
+    let config = load_config();
+
+    let app = Router::new()
+        .route("/", get(index))
+        .layer(TraceLayer::new_for_http());
+
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", config.address, config.port))
         .await
         .expect("Failed to bind to address");
-    println!("Listening on {}", listener.local_addr().unwrap());
+
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Config {
+    address: String,
+    port: u16,
+}
+
+fn load_config() -> Config {
+    let env_path = dotenvy::dotenv().expect(".env variable should exist in working directory");
+    tracing::info!("Loaded .env successfully from {}", env_path.display());
+
+    let config = envy::from_env::<Config>().expect("Failed to deserialize env variables");
+    tracing::debug!("Config: {:#?}", config);
+
+    return config;
 }
 
 async fn index() -> Html<&'static str> {
